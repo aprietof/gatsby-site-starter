cf0881fb841ca7f467da07f9a9568a6115757453
Adrian Prieto <adrianprieto00@gmail.com>
2019-09-14 12:58:46 -0400

    Update config file

--- config/website.js
+++ config/website.js
@@ -1,20 +1,19 @@
 module.exports = {
   _pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  _title: 'Andres en Ingles', // Navigation and Site Title
-  _titleAlt: 'Andres en Ingles', // Title for JSONLD
-  description:
-    'Andres Franklin is an award winning colombian youtuber and content creator based in New York city.',
-  _url: 'https://andreseningles.net', // Domain of your site. No trailing slash!
+  _title: 'Site title', // Navigation and Site Title
+  _titleAlt: 'Site title', // Title for JSONLD
+  description: 'Site description',
+  _url: 'https://siteurl.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   logo: '/logos/card.jpg', // Used for SEO
 
   // JSONLD / Manifest
   favicon: 'src/images/favicon.png', // Used for manifest favicon generation
-  shortName: 'MDX Starter', // shortname for manifest. MUST be shorter than 12 characters
-  author: 'andreseningles', // Author for schemaORGJSONLD
+  shortName: 'Short site title', // shortname for manifest. MUST be shorter than 12 characters
+  author: 'author', // Author for schemaORGJSONLD
   themeColor: '#3D63AE',
   backgroundColor: '#EBEDF2',
 
-  twitter: '@andreseningles_', // Twitter Username
+  twitter: '@twitterusername', // Twitter Username
   twitterCardType: 'summary_large_image', // Twitter card type
 };
